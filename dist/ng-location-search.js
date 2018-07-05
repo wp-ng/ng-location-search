00f1661b1e15976d72887e516aad790cd67cef71
auban <auban@stag-agency.be>
2018-07-05 10:12:14 +0200

    Add ng-location-search-default for set default value other than null

--- dist/ng-location-search.js
+++ dist/ng-location-search.js
@@ -5,6 +5,7 @@
             restrict: "A",
             require: [ "?ngModel", "?^form" ],
             link: function(scope, elem, attrs, Ctrl) {
+                var default_value = attrs.ngLocationSearchDefault || null;
                 var search_keys;
                 var modelCtrl = Ctrl[0];
                 var formCtrl = Ctrl[1];
@@ -20,18 +21,19 @@
                     var reset_search = scope.$eval(attrs.ngLocationSearchReset);
                     var location_href = trailling_slash(attrs.ngLocationSearchUrl);
                     var abs_url = trailling_slash($window.location.href.replace($window.location.hash, ""));
+                    var delay_change = attrs.ngLocationSearchDelay || 0;
                     try {
                         var value_object = angular.fromJson(value);
                         value = value_object;
                     } catch (err) {}
                     angular.forEach(search_keys, function(key, val) {
-                        var search_val = null;
+                        var search_val = default_value;
                         if (angular.isObject(value) && angular.isDefined(value[key])) {
                             search_val = value[key];
                         } else if (angular.isString(value)) {
                             search_val = value;
                         }
-                        new_search[key] = search_val && search_val !== "" ? search_val.toString() : null;
+                        new_search[key] = search_val && search_val !== "" ? search_val.toString() : default_value;
                     });
                     var current_search = $location.search();
                     if (!reset_search) {
@@ -41,58 +43,55 @@
                         return;
                     }
                     $rootScope.$broadcast("ngLocationSearchChangeStart", new_search, current_search);
-                    if (angular.isString(location_href) && location_href !== abs_url) {
-                        var is_add_to_url = location_href.indexOf("/") === 0 ? false : true;
-                        var is_internal_url = location_href.indexOf("#!/") !== -1 || location_href.indexOf("#/") !== -1;
-                        var is_abs_url = location_href.indexOf($location.protocol()) === 0;
-                        var path_url = $location.path();
-                        var hash_url = trailling_slash($window.location.hash.replace($location.url(), ""));
-                        var param_url = $httpParamSerializer(new_search);
-                        var new_href = "";
-                        if (!$location.$$html5 || is_abs_url) {
-                            param_url = param_url ? "?" + param_url : "";
-                            new_href = location_href + param_url;
-                            if (!is_internal_url && is_abs_url) {
-                                path_url = path_url && !$location.$$html5 ? "/" : "";
-                                new_href = location_href + hash_url + path_url + param_url;
-                            }
-                            $window.location.href = new_href;
-                        } else {
-                            $location.search(param_url);
-                            if (is_replace_search) {
+                    $timeout(function() {
+                        if (angular.isString(location_href) && location_href !== abs_url) {
+                            var is_add_to_url = location_href.indexOf("/") === 0 ? false : true;
+                            var is_internal_url = location_href.indexOf("#!/") !== -1 || location_href.indexOf("#/") !== -1;
+                            var is_abs_url = location_href.indexOf($location.protocol()) === 0;
+                            var path_url = $location.path();
+                            var hash_url = trailling_slash($window.location.hash.replace($location.url(), ""));
+                            var param_url = $httpParamSerializer(new_search);
+                            var new_href = "";
+                            if (!$location.$$html5 || is_abs_url) {
+                                param_url = param_url ? "?" + param_url : "";
+                                new_href = location_href + param_url;
+                                if (!is_internal_url && is_abs_url) {
+                                    path_url = path_url && !$location.$$html5 ? "/" : "";
+                                    new_href = location_href + hash_url + path_url + param_url;
+                                }
+                                $window.location.href = new_href;
+                            } else {
+                                $location.search(param_url);
+                                if (is_replace_search) {
+                                    scope.$apply();
+                                }
+                                if (is_add_to_url) {
+                                    var _path_urls = path_url.split("/");
+                                    if (_path_urls[_path_urls.length - 1] === location_href) {
+                                        new_href = path_url;
+                                    } else {
+                                        new_href = path_url + "/" + location_href;
+                                    }
+                                } else {
+                                    new_href = location_href;
+                                }
+                                $location.path(new_href);
                                 scope.$apply();
                             }
-                            if (is_add_to_url) {
-                                var _path_urls = path_url.split("/");
-                                if (_path_urls[_path_urls.length - 1] === location_href) {
-                                    new_href = path_url;
-                                } else {
-                                    new_href = path_url + "/" + location_href;
-                                }
-                            } else {
-                                new_href = location_href;
-                            }
-                            $location.path(new_href);
-                            scope.$apply();
                         }
-                        $log.debug(new_href);
+                        $location.search(new_search);
                         $rootScope.$broadcast("ngLocationSearchChangeSuccess", new_search, current_search);
-                    } else {
-                        $timeout(function() {
-                            $location.search(new_search);
-                            $rootScope.$broadcast("ngLocationSearchChangeSuccess", new_search, current_search);
-                        });
-                    }
+                    }, parseInt(delay_change, 10));
                 }
                 if (attrs.ngLocationSearch && (modelCtrl || formCtrl)) {
                     search_keys = scope.$eval(attrs.ngLocationSearch);
                     search_keys = angular.isArray(search_keys) ? search_keys : [ attrs.ngLocationSearch ];
-                    var search = null;
+                    var search = default_value;
                     var changeModel = function(loc_search) {
-                        search = search_keys.length > 1 ? {} : null;
+                        search = search_keys.length > 1 ? {} : default_value;
                         angular.forEach(search_keys, function(key, val) {
                             if (angular.isObject(search)) {
-                                search[key] = angular.isDefined(loc_search[key]) ? loc_search[key] : null;
+                                search[key] = angular.isDefined(loc_search[key]) ? loc_search[key] : default_value;
                             } else if (angular.isDefined(loc_search[key])) {
                                 search = loc_search[key];
                             }
--- src/ng-location-search.js
+++ src/ng-location-search.js
@@ -15,6 +15,7 @@
                 require: ['?ngModel', '?^form'],
                 link: function (scope, elem, attrs, Ctrl) {
 
+                    var default_value = attrs.ngLocationSearchDefault || null;
                     var search_keys;
 
                     /**
@@ -51,6 +52,7 @@
                         var reset_search = scope.$eval(attrs.ngLocationSearchReset);
                         var location_href = trailling_slash(attrs.ngLocationSearchUrl);
                         var abs_url = trailling_slash($window.location.href.replace($window.location.hash, ''));
+                        var delay_change = attrs.ngLocationSearchDelay || 0;
 
                         //Deserializes a JSON search string.
                         try {
@@ -64,7 +66,7 @@
 
                         //Apply key val on location search.
                         angular.forEach(search_keys, function (key, val) {
-                            var search_val = null;
+                            var search_val = default_value;
 
                             if (angular.isObject(value) && angular.isDefined(value[key]) ) {
                                 search_val = value[key];
@@ -73,7 +75,7 @@
                                 search_val = value;
                             }
 
-                            new_search[key] = (search_val && search_val !== '') ? search_val.toString() : null;
+                            new_search[key] = (search_val && search_val !== '') ? search_val.toString() : default_value;
                         });
 
                         var current_search = $location.search();
@@ -89,69 +91,66 @@
 
                         $rootScope.$broadcast('ngLocationSearchChangeStart', new_search, current_search);
 
-                        //Redirect to search url
-                        if (angular.isString(location_href) && location_href !== abs_url) {
+                        $timeout(function () {
 
-                            var is_add_to_url = (location_href.indexOf('/') === 0) ? false : true;
-                            var is_internal_url = location_href.indexOf('#!/') !== -1 || location_href.indexOf('#/') !== -1;
-                            var is_abs_url = location_href.indexOf($location.protocol()) === 0;
 
-                            var path_url = $location.path();
-                            var hash_url = trailling_slash($window.location.hash.replace($location.url(), ''));
-                            var param_url = $httpParamSerializer(new_search);
+                            //Redirect to search url
+                            if (angular.isString(location_href) && location_href !== abs_url) {
 
-                            var new_href = '';
+                                var is_add_to_url = (location_href.indexOf('/') === 0) ? false : true;
+                                var is_internal_url = location_href.indexOf('#!/') !== -1 || location_href.indexOf('#/') !== -1;
+                                var is_abs_url = location_href.indexOf($location.protocol()) === 0;
 
-                            if (!$location.$$html5 || is_abs_url) {
+                                var path_url = $location.path();
+                                var hash_url = trailling_slash($window.location.hash.replace($location.url(), ''));
+                                var param_url = $httpParamSerializer(new_search);
 
-                                param_url = param_url ? '?' + param_url : '';
-                                new_href = location_href + param_url;
+                                var new_href = '';
 
-                                if (!is_internal_url && is_abs_url) {
-                                    path_url = path_url && !$location.$$html5 ? '/' : '';
-                                    new_href = location_href + hash_url + path_url + param_url ;
-                                }
+                                if (!$location.$$html5 || is_abs_url) {
 
-                                $window.location.href = new_href;
-                            }
-                            else {
+                                    param_url = param_url ? '?' + param_url : '';
+                                    new_href = location_href + param_url;
 
-                                $location.search(param_url);
-
-                                if (is_replace_search) {
-                                    scope.$apply();
-                                }
-
-                                if (is_add_to_url) {
-
-                                    var _path_urls = path_url.split('/');
-
-                                    //Prevent same last for relative url
-                                    if ( _path_urls[_path_urls.length -1] === location_href) {
-                                        new_href = path_url;
+                                    if (!is_internal_url && is_abs_url) {
+                                        path_url = path_url && !$location.$$html5 ? '/' : '';
+                                        new_href = location_href + hash_url + path_url + param_url ;
                                     }
-                                    else {
-                                        new_href = path_url + "/" + location_href;
-                                    }
+
+                                    $window.location.href = new_href;
                                 }
                                 else {
-                                    new_href = location_href;
-                                }
 
-                                $location.path(new_href);
-                                scope.$apply();
+                                    $location.search(param_url);
+
+                                    if (is_replace_search) {
+                                        scope.$apply();
+                                    }
+
+                                    if (is_add_to_url) {
+
+                                        var _path_urls = path_url.split('/');
+
+                                        //Prevent same last for relative url
+                                        if ( _path_urls[_path_urls.length -1] === location_href) {
+                                            new_href = path_url;
+                                        }
+                                        else {
+                                            new_href = path_url + "/" + location_href;
+                                        }
+                                    }
+                                    else {
+                                        new_href = location_href;
+                                    }
+
+                                    $location.path(new_href);
+                                    scope.$apply();
+                                }
                             }
 
-                            $log.debug(new_href);
-
+                            $location.search(new_search);
                             $rootScope.$broadcast('ngLocationSearchChangeSuccess', new_search, current_search);
-                        }
-                        else {
-                            $timeout(function() {
-                                $location.search(new_search);
-                                $rootScope.$broadcast('ngLocationSearchChangeSuccess', new_search, current_search);
-                            });
-                        }
+                        }, parseInt(delay_change, 10));
                     }
 
                     //Use the current model scope.
@@ -160,17 +159,17 @@
 
                         search_keys = scope.$eval(attrs.ngLocationSearch);
                         search_keys = angular.isArray(search_keys) ? search_keys : [attrs.ngLocationSearch];
-                        var search = null;
+                        var search = default_value;
 
                         //Set the model change from location search object.
                         var changeModel = function (loc_search) {
-                            search = (search_keys.length > 1) ? {} : null;
+                            search = (search_keys.length > 1) ? {} : default_value;
 
                             //Find in url search params
                             angular.forEach(search_keys, function (key, val) {
 
                                 if (angular.isObject(search)) {
-                                    search[key] = angular.isDefined(loc_search[key]) ? loc_search[key] : null;
+                                    search[key] = angular.isDefined(loc_search[key]) ? loc_search[key] : default_value;
                                 }
                                 else if (angular.isDefined(loc_search[key])) {
                                     search = loc_search[key];
